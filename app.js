25406beb371854c07ae16889c8e36bacf978c25c
samrit <samrit.dhesi@hotmail.com>
2020-02-15 20:56:04 -0800

    Added image initialization

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const port = 3000
 app.set('view engine', 'ejs');
 app.set("views","./views");
 
-
-
 require("firebase/auth");
 require("firebase/firestore");
 
@@ -31,14 +29,13 @@ firebase.initializeApp(firebaseConfig);
 
 var serviceAccount = require('./DB/adminkey.json');
 
-
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://vot3s-da1d7.firebaseio.com"
 });
 
 let db = admin.firestore();
+
 /*
 let test = db.collection('users').doc("testdoc");
 let setTest = test.set({
@@ -46,7 +43,6 @@ let setTest = test.set({
     next: 'test1'
 });
 */
-
 
 app.get('/', (req, res) => {
   res.render('index', {title: "DON'T HACK", user: "Me"})
@@ -64,5 +60,21 @@ io.on('connection', function(socket){
   });
 });
 
+//just add
+const testFolder = './images/';
+const fs = require('fs');
+
+// put images in array
+var myfile;
+var filenames = [];
+var i = 0;
+
+fs.readdir(testFolder, (err, files) => {
+    files.forEach(file => {
+    filenames.push(file);
+    console.log(filenames[i]);
+    i++;
+    });
+})
 
 http.listen(port, () => console.log(`Example app listening on port ${port}!`))
