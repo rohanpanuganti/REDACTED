95ec2133fbfe2cdfac98707eb1fa225620f14d53
Joel Davidson <davids24@seattleu.edu>
2020-02-15 16:32:24 -0800

    Successfully set up and communicated with firebase datastore

--- app.js
+++ app.js
@@ -11,11 +11,16 @@ app.get('/', (req, res) => {
     res.render('index', {title: "DON'T HACK", user: "Me"})
 });
 
-
-var firebase = require("firebase/app");
-var admin = require("firebase-admin");
 require("firebase/auth");
 require("firebase/firestore");
+
+var firebase = require("firebase/app");
+const admin = require("firebase-admin");
+const functions = require("firebase-functions");
+
+
+//admin.initializeApp(functions.config().firebase);
+
 
 var serviceAccount = require(".\\DB\\adminkey.json");
 
@@ -24,6 +29,14 @@ var serviceAccount = require(".\\DB\\adminkey.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://vot3s-da1d7.firebaseio.com"
+});
+
+let db = admin.firestore();
+
+let test = db.collection('users').doc("testdoc");
+let setTest = test.set({
+    first: 'Yeah',
+    next: 'Two'
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
